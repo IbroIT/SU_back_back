c00094fbb5af6df0856986206af95759d5ce7948
adilhan <adilhansatymkulov40@gmail.com>
2025-09-19 18:31:22 +0600

    commit

--- student_life/serializers.py
+++ student_life/serializers.py
@@ -1,15 +1,23 @@
 from rest_framework import serializers
-from .models import PartnerOrganization, StudentAppeal
+from .models import PartnerOrganization, StudentAppeal, OrganizationSpecialization
+
+
+class OrganizationSpecializationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrganizationSpecialization
+        fields = ['id', 'name_ru', 'name_kg', 'name_en']
 
 
 class PartnerOrganizationSerializer(serializers.ModelSerializer):
+    specializations = OrganizationSpecializationSerializer(many=True, read_only=True)
+    
     class Meta:
         model = PartnerOrganization
         fields = [
             'id', 'name_ru', 'name_kg', 'name_en',
             'description_ru', 'description_kg', 'description_en',
             'type', 'location', 'contact_person', 'phone', 'email', 
-            'website', 'is_active'
+            'website', 'is_active', 'specializations'
         ]
 
 
