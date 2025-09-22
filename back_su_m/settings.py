def86f813d858716d59ab03e62a08de0657d1354
Ibro <ibroabdraimov20@gmail.com>
2025-09-22 19:36:20 +0600

    asdasd

--- back_su_m/settings.py
+++ back_su_m/settings.py
@@ -173,7 +173,9 @@ REST_FRAMEWORK = {
 # -------------------
 # CORS
 # -------------------
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5174",
+]
 CORS_ALLOW_CREDENTIALS = True
 
 # -------------------
